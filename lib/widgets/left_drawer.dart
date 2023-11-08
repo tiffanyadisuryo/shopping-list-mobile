52c58498c5e5571fd5f164806745773b3381e870
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2023-11-08 14:40:00 +0700

    tambahan

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -29,6 +29,12 @@ class LeftDrawer extends StatelessWidget {
                 Padding(padding: EdgeInsets.all(10)),
                 Text("Catat seluruh keperluan belanjamu di sini!",
                     // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
                     ),
               ],
             ),
